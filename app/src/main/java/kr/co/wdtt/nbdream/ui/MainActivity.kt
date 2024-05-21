36d9cee86cc492d43dfd6e18eabaf161e33510b0
guri999 <zaq8644@gmail.com>
2024-05-22 04:37:31 +0900

    Feat: Application 클래스 추가 Hilt

--- app/src/main/java/kr/co/wdtt/nbdream/ui/MainActivity.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/ui/MainActivity.kt
@@ -6,16 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import kr.co.wdtt.nbdream.ui.DreamApp
+import dagger.hilt.android.AndroidEntryPoint
 import kr.co.wdtt.nbdream.ui.theme.NBDreamTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/kr/co/wdtt/nbdream/ui/MainViewModel.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/ui/MainViewModel.kt
@@ -1,8 +1,10 @@
 package kr.co.wdtt.nbdream.ui
 
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 //TODO 로그인 확인 온보딩 or 홈 화면 이동
+@HiltViewModel
 class MainViewModel @Inject constructor(
 
 ): BaseViewModel() {
