cc9ad535098b47f8c98fb228b9487d45bf0e55d2
guri999 <zaq8644@gmail.com>
2024-05-22 00:35:38 +0900

    Feat: 기본 프로젝트 생성

---
+++ app/src/main/java/kr/co/wdtt/nbdream/ui/MainActivity.kt
@@ -0,0 +1,34 @@
+package kr.co.wdtt.nbdream.ui
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.co.wdtt.nbdream.ui.DreamApp
+import kr.co.wdtt.nbdream.ui.theme.NBDreamTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                scrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            )
+        )
+        setContent {
+            NBDreamTheme {
+                DreamApp()
+            }
+        }
+    }
+}
