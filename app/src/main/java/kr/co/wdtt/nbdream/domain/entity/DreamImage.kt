2f7fa713088c72b5aa54600386971163a75651d2
sunjoolee <sheisthepoem@naver.com>
2024-05-25 14:35:19 +0900

    Feat: DreamImage 생성 및 이미지 필드 조회

--- app/src/main/java/kr/co/wdtt/nbdream/domain/entity/DreamImage.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/domain/entity/DreamImage.kt
@@ -1,8 +1,23 @@
 package kr.co.wdtt.nbdream.domain.entity
 
+import kr.co.wdtt.nbdream.ui.theme.ColorSet
 import java.io.File
 
-sealed class DreamImage{
-    data class RequestImage(val image: File):DreamImage()
-    data class ResponseImage(val image:String):DreamImage()
+sealed class DreamImage private constructor(){
+    companion object{
+        fun <T> create(image:T): DreamImage = when(image){
+            is File -> RequestImage(image)
+            is String -> ResponseImage(image)
+            else -> throw IllegalArgumentException("Unknown type")
+        }
+    }
+    abstract fun getImage(): Any
+
+    private data class RequestImage(private val image: File):DreamImage(){
+        override fun getImage(): File= image
+    }
+    private data class ResponseImage(private val image:String):DreamImage(){
+        override fun getImage(): String = image
+    }
 }
+
