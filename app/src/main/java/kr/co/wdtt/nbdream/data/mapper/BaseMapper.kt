485abcebfd6a3aa4ea156eab13ecb498d0d5eed1
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-05-26 02:13:20 +0900

    refactor: BaseMapper flow로

--- app/src/main/java/kr/co/wdtt/nbdream/data/mapper/BaseMapper.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/data/mapper/BaseMapper.kt
@@ -1,16 +1,17 @@
 package kr.co.wdtt.nbdream.data.mapper
 
+import kotlinx.coroutines.flow.Flow
 import kr.co.wdtt.nbdream.data.remote.model.ApiResponse
 import kr.co.wdtt.nbdream.data.remote.model.ApiResult
 
 abstract class BaseMapper<MODEL, ENTITY> {
-    fun mapFromResult(result: ApiResult<MODEL>, extra: Any? = null): EntityWrapper<ENTITY> =
-        when(val reponse = result.response) {
-            is ApiResponse.Success -> getSuccess(reponse.data, extra)
-            is ApiResponse.Fail -> getFailure(reponse.error)
+    fun mapFromResult(result: ApiResult<MODEL>, extra: Any? = null): Flow<EntityWrapper<ENTITY>> =
+        when(val response = result.response) {
+            is ApiResponse.Success -> getSuccess(response.data, extra)
+            is ApiResponse.Fail -> getFailure(response.error)
         }
 
-    abstract fun getSuccess(data: MODEL, extra: Any?): EntityWrapper.Success<ENTITY>
+    abstract fun getSuccess(data: MODEL?, extra: Any?): Flow<EntityWrapper.Success<ENTITY>>
 
-    abstract fun getFailure(error: Throwable): EntityWrapper.Fail<ENTITY>
+    abstract fun getFailure(error: Throwable): Flow<EntityWrapper.Fail<ENTITY>>
 }
