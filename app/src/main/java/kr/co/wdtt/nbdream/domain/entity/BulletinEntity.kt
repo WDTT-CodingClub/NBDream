838e907843759a1b9df2a083d2b86cec02bcb2db
jericho31 <a@a.a>
2024-05-27 13:18:27 +0900

    Feat: 커뮤니티 entity 수정

--- app/src/main/java/kr/co/wdtt/nbdream/domain/entity/BulletinEntity.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/domain/entity/BulletinEntity.kt
@@ -3,21 +3,22 @@ package kr.co.wdtt.nbdream.domain.entity
 data class BulletinEntity(
     val id: String,  // 게시글 id가 필요할지 안할지? 필요하다면 어떤 식으로 만들지?
     val content: String,
-    val pictures: List<String>,
-    val plant: Plant,
+    val pictures: List<String> = emptyList(),
+    val plant: Plant = Plant.None,
     val bulletinCategory: BulletinCategory,
     val createdTime: String,
-    val deletedTime: String,
-    val modifiedTime: String,
+    val deletedTime: String? = null,
+    val modifiedTime: String? = null,
     val userId: String,
-    val bookmarkedUsers: List<String>,
-    val comments: List<String>,  // 댓글 id만 들고 통신할지, 데이터 들고있을지?
-    val bulletinSortType: BulletinSortType,  // 댓글 id만 들고 통신할지, 데이터 들고있을지?
+    val bookmarkedUsers: List<String> = emptyList(),
+    val comments: List<String> = emptyList(),  // 댓글 id만 들고 통신할지, 데이터 들고있을지?
+    val bulletinSortType: BulletinSortType = BulletinSortType.Recent,  // 댓글 id만 들고 통신할지, 데이터 들고있을지?
 )
 
 enum class Plant(
     val value: String,  // value가 필요 없을지도
 ) {
+    None("미분류"),
     RedPepper("고추"),
     Rice("벼"),
     Potato("감자"),
@@ -40,9 +41,9 @@ enum class BulletinSortType {
 // 나중에 파일 나눌거. 일단 여기에 작성
 data class CommentEntity(
     val id: String,  // 댓글 id?
+    val userId: String,
     val content: String,
     val createdTime: String,
-    val deletedTime: String,
-    val modifiedTime: String,  // 댓글 수정은 가능하게 할지?
-    val userId: String,
+    val deletedTime: String? = null,
+    val modifiedTime: String? = null,  // 댓글 수정은 가능하게 할지?
 )
