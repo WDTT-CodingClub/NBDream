368b9b9f7343843060cf3c31c3d3bb28f08d8d0d
nhkim <nhkim1529@gmail.com>
2024-05-29 12:08:35 +0900

    Feat: UserEntity에 accessToken, refreshToken 추가

--- app/src/main/java/kr/co/wdtt/nbdream/domain/entity/UserEntity.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/domain/entity/UserEntity.kt
@@ -4,6 +4,8 @@ data class UserEntity(
     val name: String = "말하는 감자",
     val address: String? = null,
     val crops: List<DreamCrop> = emptyList(),
-    val profileImage: String = "",
-    val alarmSetting: Boolean = true
+    val profileImage: String? = null,
+    val alarmSetting: Boolean = true,
+    val accessToken: String? = null,
+    val refreshToken: String? = null
 )
