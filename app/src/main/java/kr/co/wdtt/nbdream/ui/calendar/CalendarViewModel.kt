b6d5a01306f9a274ffb2f3ab5ed2caf1dfdaa974
sunjoolee <sheisthepoem@naver.com>
2024-05-29 13:53:02 +0900

    Fix: 캘린더 뷰모델 onError 함수 오버라이드 제거

--- app/src/main/java/kr/co/wdtt/nbdream/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/ui/calendar/CalendarViewModel.kt
@@ -4,28 +4,10 @@ import android.util.Log
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import kr.co.wdtt.core.ui.base.BaseViewModel
-import kr.co.wdtt.core.ui.base.CustomErrorType
-import kr.co.wdtt.nbdream.domain.usecase.GetFarmWorkUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class CalendarViewModel @Inject constructor(
-    private val getFarmWorkUseCase:GetFarmWorkUseCase
 ) : BaseViewModel() {
     private val TAG = this@CalendarViewModel::class.java.simpleName
-
-    override suspend fun onError(errorType: CustomErrorType) {
-        //TODO("Not yet implemented")
-    }
-    init {
-        viewModelScopeEH.launch {
-            farmWorkApiTest()
-        }
-    }
-
-    private suspend fun farmWorkApiTest() {
-        getFarmWorkUseCase.invoke("30697").collect{
-            Log.d(TAG, "$it")
-        }
-    }
 }
