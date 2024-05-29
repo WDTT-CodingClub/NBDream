e4c636870a9e4e236b72aa3887c3927ed9e6dd53
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-05-30 07:42:43 +0900

    Feat: Token저장 DataStore생성
    saveUserId 유저 아이디 저장,
    fetchUserId 저장된 유저 아이디 가져옴, 이거 통해서 스플래시 화면 진입점 정하면 됨
    
    액세스 토큰, 유저가 서버로 진입하는 토큰 (cookie로 변경될 수 있음)
    
    리프레시 토큰 (유통기간, 기간 지나면 진입 안됨, 새로 리프레시 해줘야함)

--- app/src/main/java/kr/co/wdtt/nbdream/ui/DreamApp.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/ui/DreamApp.kt
@@ -2,7 +2,6 @@ package kr.co.wdtt.nbdream.ui
 
 import androidx.compose.runtime.Composable
 import kr.co.wdtt.nbdream.ui.calendar.CalendarScreen
-import kr.co.wdtt.nbdream.ui.home.HomeRoute
 
 @Composable
 fun DreamApp(){
