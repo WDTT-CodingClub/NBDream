10cc34277d890af123f0fa3d5e257e1241e66ebc
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-05-31 04:53:00 +0900

    Feat: Ktor api연결 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,4 +113,18 @@ dependencies {
 
     // data store
     implementation(libs.androidx.datastore.preferences)
+
+    //ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.darwin)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.auth)
+
+    //timber
+    implementation (libs.timber)
 }
