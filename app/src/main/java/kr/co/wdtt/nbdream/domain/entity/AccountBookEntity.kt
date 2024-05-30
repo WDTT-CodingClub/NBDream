0eb82c2e12b126438d752b4fd192a0b77eddff69
s-subeen <101384641+s-subeen@users.noreply.github.com>
2024-05-30 09:39:55 +0900

    Refactor: Category enum 값을 Int에서 String으로 변경

--- app/src/main/java/kr/co/wdtt/nbdream/domain/entity/AccountBookEntity.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/domain/entity/AccountBookEntity.kt
@@ -1,8 +1,5 @@
 package kr.co.wdtt.nbdream.domain.entity
 
-import androidx.annotation.StringRes
-import kr.co.wdtt.nbdream.R
-
 data class AccountBookEntity(
     val id: String,
     val userId: String? = null, // 응답 시 불필요
@@ -20,20 +17,20 @@ data class AccountBookEntity(
     val totalRevenue: Long? = null, // 총 수입
     val totalExpense: Long? = null, // 총 지출
     val totalCost: Long? = null,  // 총 비용
-)
-
-enum class Category(
-    @StringRes val category: Int
 ) {
-    FARM_PRODUCT_SALES(R.string.farm_product_sales),
-    FERTILIZER(R.string.fertilizer),
-    SEEDS_AND_SEEDLINGS(R.string.seeds_and_seedlings),
-    PESTICIDES(R.string.pesticides),
-    FARM_MACHINERY(R.string.farm_machinery),
-    DISTRIBUTION_EXPENSE(R.string.distribution_expense),
-    RENT(R.string.rent),
-    SEED_SALES(R.string.seed_sales),
-    FARM_EQUIPMENT_USAGE_FEE(R.string.farm_equipment_usage_fee),
-    TENT_RENTAL(R.string.farm_equipment_rental),
-    OTHER(R.string.other),
+    enum class Category(
+        val value: String
+    ) {
+        FARM_PRODUCT_SALES("농산물 판매"),
+        FERTILIZER("비료"),
+        SEEDS_AND_SEEDLINGS("농자/종묘"),
+        PESTICIDES("농약"),
+        FARM_MACHINERY("농기계"),
+        DISTRIBUTION_EXPENSE("유통비"),
+        RENT("임차료"),
+        SEED_SALES("종자 판매"),
+        FARM_EQUIPMENT_USAGE_FEE("농기구 사용료"),
+        FARM_EQUIPMENT__RENTAL("농기구 대여"),
+        OTHER("기타"),
+    }
 }
