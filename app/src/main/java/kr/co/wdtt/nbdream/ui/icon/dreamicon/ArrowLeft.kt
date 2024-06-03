ced51cc57a5e20d7e371c3979622c88b2ec66522
sunjoolee <sheisthepoem@naver.com>
2024-06-03 11:26:45 +0900

    Design: 캘린더 화면 사용 아이콘 추가

---
+++ app/src/main/java/kr/co/wdtt/nbdream/ui/icon/dreamicon/ArrowLeft.kt
@@ -0,0 +1,32 @@
+package kr.co.wdtt.nbdream.ui.icon.dreamicon
+
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import kr.co.wdtt.nbdream.ui.icon.dreamicon.DreamIcon
+
+public val DreamIcon.ArrowLeft: ImageVector
+    get() {
+        if (_arrowLeft != null) {
+            return _arrowLeft!!
+        }
+        _arrowLeft = Builder(name = "ArrowLeft", defaultWidth = 28.0.dp, defaultHeight = 24.0.dp,
+                viewportWidth = 28.0f, viewportHeight = 24.0f).apply {
+            path(fill = null, stroke = null, strokeLineWidth = 0.0f, strokeLineCap = Butt,
+                    strokeLineJoin = Miter, strokeLineMiter = 4.0f, pathFillType = NonZero) {
+                moveTo(0.0f, 24.0f)
+                lineToRelative(28.0f, -0.0f)
+                lineToRelative(0.0f, -24.0f)
+                lineTo(0.0f, 0.0f)
+                close()
+            }
+        }
+        .build()
+        return _arrowLeft!!
+    }
+
+private var _arrowLeft: ImageVector? = null
