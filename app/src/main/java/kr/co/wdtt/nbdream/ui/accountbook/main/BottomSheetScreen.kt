ad1a43fdbeec84008af89fd7bc799ac14bbcb82b
s-subeen <101384641+s-subeen@users.noreply.github.com>
2024-06-04 17:48:21 +0900

    Feat: 카테고리 BottomSheet 추가

---
+++ app/src/main/java/kr/co/wdtt/nbdream/ui/accountbook/main/BottomSheetScreen.kt
@@ -0,0 +1,99 @@
+package kr.co.wdtt.nbdream.ui.accountbook.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
+import kr.co.wdtt.nbdream.ui.theme.colors
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetScreen(
+    onSelectedListener: (String) -> Unit,
+    categories: List<String>,
+    dismissBottomSheet: () -> Unit,
+) {
+    var selectedCategory by remember { mutableStateOf<String?>(null) }
+
+    val sheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        onDismissRequest = dismissBottomSheet,
+        sheetState = sheetState,
+        shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+        contentColor = Color.White,
+        dragHandle = null,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val contentHeight = getContentHeight(categories.size)
+
+        Column(
+            modifier = Modifier
+                .background(color = Color.White)
+                .padding(16.dp)
+                .height(contentHeight),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = "카테고리를 선택하세요",
+                fontSize = 18.sp,
+                style = TextStyle(fontWeight = FontWeight.Bold),
+                textAlign = TextAlign.Start,
+                modifier = Modifier.padding(bottom = 12.dp),
+                color = Color.Black
+            )
+            categories.forEach { category ->
+                val backgroundColor = if (category == selectedCategory) MaterialTheme.colors.primary else Color.Transparent
+                Text(
+                    text = category,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            selectedCategory = category
+                            onSelectedListener(category)
+                        }
+                        .background(color = backgroundColor)
+                        .padding(vertical = 8.dp),
+                    textAlign = TextAlign.Start,
+                    color = Color.Black
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+fun getContentHeight(numOfItems: Int): Dp {
+    val itemHeight = 50.dp
+    val verticalPadding = 16.dp
+    val bottomBarHeight = 56.dp
+    val totalContentHeight = numOfItems * itemHeight + verticalPadding * 2 + bottomBarHeight
+    return totalContentHeight
+}
