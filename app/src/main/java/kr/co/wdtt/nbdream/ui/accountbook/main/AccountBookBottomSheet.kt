aade46b0a618395fb0a3d09b94fb5f954452573f
s-subeen <101384641+s-subeen@users.noreply.github.com>
2024-06-04 23:16:16 +0900

    Refactor: 미사용 import 제거

--- app/src/main/java/kr/co/wdtt/nbdream/ui/accountbook/main/AccountBookBottomSheet.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/ui/accountbook/main/AccountBookBottomSheet.kt
@@ -26,10 +26,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.times
 import kr.co.wdtt.nbdream.ui.theme.colors
 
 @OptIn(ExperimentalMaterial3Api::class)
