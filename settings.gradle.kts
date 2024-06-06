e2b576c0540273bbd200d91f3ba9c33a95843dd3
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-06-07 07:42:15 +0900

    Fix: Kakao sdk 의존성 문제, kakao maven 저장소 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
 
