e0147e76999ad8690488387946e48784ea69de4e
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-06-07 11:34:09 +0900

    Feat: Font추가

--- core/ui/src/main/java/kr/co/ui/theme/Typo.kt
+++ core/ui/src/main/java/kr/co/ui/theme/Typo.kt
@@ -1,16 +1,72 @@
-package kr.co.wdtt.nbdream.ui.theme
+package kr.co.ui.theme
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import kr.co.nbdream.core.ui.R
+
+private val DreamFontFamily = FontFamily(
+    Font(R.font.pretendard_black, weight = FontWeight.Black),
+    Font(R.font.pretendard_bold, weight = FontWeight.Bold),
+    Font(R.font.pretendard_extrabold, weight = FontWeight.ExtraBold),
+    Font(R.font.pretendard_extralight, weight = FontWeight.ExtraLight),
+    Font(R.font.pretendard_light, weight = FontWeight.Light),
+    Font(R.font.pretendard_medium, weight = FontWeight.Medium),
+    Font(R.font.pretendard_regular, weight = FontWeight.Normal),
+    Font(R.font.pretendard_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.pretendard_thin, weight = FontWeight.Thin),
+)
 
 // Set of Material typography styles to start with
 val Typography = DreamTypography(
+    h1 = TextStyle(
+        fontFamily = DreamFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = TextUnit.Unspecified
+    ),
+    h2 = TextStyle(
+        fontFamily = DreamFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = TextUnit.Unspecified
+    ),
+    h3 = TextStyle(
+        fontFamily = DreamFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = TextUnit.Unspecified * 1.4,
+        letterSpacing = (-0.01).em
+    ),
+    body1 = TextStyle(
+        fontFamily = DreamFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = TextUnit.Unspecified * 1.6,
+        letterSpacing = (-0.01).em
+    ),
+    body2 = TextStyle(
+        fontFamily = DreamFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = TextUnit.Unspecified * 1.6,
+        letterSpacing = (-0.01).em
+    ),
+    label = TextStyle(
+        fontFamily = DreamFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        lineHeight = TextUnit.Unspecified * 1.6,
+        letterSpacing = (-0.01).em
+    ),
     displayB = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
@@ -141,6 +197,12 @@ val Typography = DreamTypography(
 
 @Immutable
 data class DreamTypography(
+    val h1: TextStyle = TextStyle.Default,
+    val h2: TextStyle = TextStyle.Default,
+    val h3: TextStyle = TextStyle.Default,
+    val body1: TextStyle = TextStyle.Default,
+    val body2: TextStyle = TextStyle.Default,
+    val label: TextStyle = TextStyle.Default,
     val displayB: TextStyle = TextStyle.Default,
     val displaySB: TextStyle = TextStyle.Default,
     val headerB: TextStyle = TextStyle.Default,
