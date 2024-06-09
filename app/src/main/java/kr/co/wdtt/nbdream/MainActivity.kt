4db82dccc39df9f62555a120ce8dae948c108adf
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-06-09 20:16:21 +0900

    workspace

--- app/src/main/java/kr/co/wdtt/nbdream/MainActivity.kt
+++ app/src/main/java/kr/co/wdtt/nbdream/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.wdtt.nbdream
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import kr.co.common.ContextManager
 import kr.co.wdtt.nbdream.ui.DreamApp
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ContextManager.setContext(this)
+        enableEdgeToEdge()
         setContent {
             NBDreamTheme {
                 DreamApp()
