6020b5a052bc8bc1170756a60c63d95f645e053f
nhkim <nhkim1529@gmail.com>
2024-06-09 23:32:32 +0900

    Chore: kakao map 의존성 추가

--- feature/onboard/build.gradle.kts
+++ feature/onboard/build.gradle.kts
@@ -1,8 +1,20 @@
 plugins {
     alias(libs.plugins.nbdream.android.feature)
     alias(libs.plugins.nbdream.android.library.compose)
+    alias(libs.plugins.sgp)
 }
 
 android {
     namespace = "kr.co.onboard"
+
+    defaultConfig {
+        buildConfigField("String", "KAKAO_API_KEY", "\"kakaoApiKey\"")
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    api(libs.kakao.maps)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,8 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
+
+        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/repository/kakaomap-releases/") }
     }
 }
 
