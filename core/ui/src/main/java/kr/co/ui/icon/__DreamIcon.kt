84ad3c685dacf571d2e9b1d647392555ae950a34
guri999 <zaq8644@gmail.com>
2024-06-10 02:20:29 +0900

    Design: Add weather Icon

--- core/ui/src/main/java/kr/co/ui/icon/__DreamIcon.kt
+++ core/ui/src/main/java/kr/co/ui/icon/__DreamIcon.kt
@@ -1,28 +1,18 @@
 package kr.co.ui.icon
 
 import androidx.compose.ui.graphics.vector.ImageVector
-import kr.co.ui.icon.dreamicon.Add
-import kr.co.ui.icon.dreamicon.Alarm
-import kr.co.ui.icon.dreamicon.ArrowLeft
-import kr.co.ui.icon.dreamicon.ArrowRight
-import kr.co.ui.icon.dreamicon.Delete
-import kr.co.ui.icon.dreamicon.Dropdown
-import kr.co.ui.icon.dreamicon.Edit
-import kr.co.ui.icon.dreamicon.Search
-import kr.co.ui.icon.dreamicon.Spinner
-import kr.co.ui.icon.dreamicon.Sprout
+import kr.co.ui.icon.dreamicon.Sunny
 import kotlin.collections.List as ____KtList
 
 public object DreamIcon
 
-private var __AllIcons: ____KtList<ImageVector>? = null
+private var __DreamIcon: ____KtList<ImageVector>? = null
 
-public val DreamIcon.AllIcons: ____KtList<ImageVector>
+public val DreamIcon.DreamIcon: ____KtList<ImageVector>
   get() {
-    if (__AllIcons != null) {
-      return __AllIcons!!
+    if (__DreamIcon != null) {
+      return __DreamIcon!!
     }
-    __AllIcons = listOf(Add, Alarm, ArrowLeft, ArrowRight, Delete, Dropdown, Edit, Search, Spinner,
-        Sprout)
-    return __AllIcons!!
+    __DreamIcon= listOf(Sunny)
+    return __DreamIcon!!
   }
