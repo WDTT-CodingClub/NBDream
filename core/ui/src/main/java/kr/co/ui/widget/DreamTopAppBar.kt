27b9e5c17263d8900e2ab7c82e446edefb722011
guri999 <zaq8644@gmail.com>
2024-06-10 02:20:57 +0900

    Design: update topAppbar action padding

--- core/ui/src/main/java/kr/co/ui/widget/DreamTopAppBar.kt
+++ core/ui/src/main/java/kr/co/ui/widget/DreamTopAppBar.kt
@@ -2,6 +2,8 @@ package kr.co.ui.widget
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -14,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import kr.co.ui.icon.DreamIcon
@@ -45,7 +48,10 @@ fun DreamTopAppBar(
         },
         colors = colors,
         navigationIcon = navigationIcon,
-        actions = actions
+        actions = {
+            actions()
+            Spacer(modifier = Modifier.width(12.dp))
+        }
     )
 }
 
