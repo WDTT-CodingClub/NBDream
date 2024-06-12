4ce612709b1fb1f7001fef323d653c7efc6080f3
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-06-13 07:44:02 +0900

    - Feat: create to MyPage/UI

--- core/ui/src/main/java/kr/co/ui/widget/DreamTopAppBar.kt
+++ core/ui/src/main/java/kr/co/ui/widget/DreamTopAppBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,6 +44,7 @@ fun DreamTopAppBar(
             Row(
                 modifier = Modifier
                     .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(
