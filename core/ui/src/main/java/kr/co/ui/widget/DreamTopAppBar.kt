73f45bf5e1ed78339799f4f61c699952d6a4fe75
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-06-13 07:00:33 +0900

    - Design: changed design of DreamTopAppBar

--- core/ui/src/main/java/kr/co/ui/widget/DreamTopAppBar.kt
+++ core/ui/src/main/java/kr/co/ui/widget/DreamTopAppBar.kt
@@ -1,58 +1,66 @@
 package kr.co.ui.widget
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
-import androidx.compose.ui.unit.sp
 import kr.co.ui.icon.DreamIcon
 import kr.co.ui.icon.dreamicon.Bell
 import kr.co.ui.theme.NBDreamTheme
 import kr.co.ui.theme.colors
 import kr.co.ui.theme.typo
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DreamTopAppBar(
     modifier: Modifier = Modifier,
     title: String,
-    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(),
-    navigationIcon: @Composable () -> Unit = {},
+    description: String? = null,
+    color: Color = Color.Transparent,
     actions: @Composable RowScope.() -> Unit = {},
 ) {
-    TopAppBar(
-        modifier = modifier,
-        title = {
-            Text(
-                text = title,
-                style = MaterialTheme.typo.h2.copy(
-                    color = MaterialTheme.colors.gray1,
-                    lineHeight = 30.sp,
-                    letterSpacing = (-0.011).em
+    Column {
+        Spacer(modifier = Modifier.height(24.dp))
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(color = color),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typo.h2
                 )
-            )
-        },
-        colors = colors,
-        navigationIcon = navigationIcon,
-        actions = {
-            actions()
-            Spacer(modifier = Modifier.width(12.dp))
+
+                actions()
+            }
+            description?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typo.body1,
+                    color = MaterialTheme.colors.gray2
+                )
+            }?: Spacer(modifier = Modifier.height(24.dp))
         }
-    )
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -62,9 +70,6 @@ private fun DreamTopAppBarPreview() {
     NBDreamTheme {
         DreamTopAppBar(
             title = "내 농장",
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = Color(0xFFF0F0F0)
-            ),
             actions = {
                 Icon(
                     imageVector = DreamIcon.Bell,
