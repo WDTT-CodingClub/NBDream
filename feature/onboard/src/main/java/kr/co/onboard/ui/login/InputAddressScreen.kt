c3be49fbf539e5693288db40032b203b67d5b6f7
nhkim <nhkim1529@gmail.com>
2024-06-13 10:25:18 +0900

    Feat: 카카오 지도 기본 상태 구현

--- feature/onboard/src/main/java/kr/co/onboard/ui/login/InputAddressScreen.kt
+++ feature/onboard/src/main/java/kr/co/onboard/ui/login/InputAddressScreen.kt
@@ -1,8 +1,9 @@
-package kr.co.onboard.ui.login
+package kr.co.onboard.login
 
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.ViewGroup
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
 import android.webkit.WebView
@@ -16,7 +17,7 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -26,9 +27,11 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -36,6 +39,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,11 +48,65 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.KakaoMapSdk
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
+import kr.co.onboard.BuildConfig
+import kr.co.onboard.ui.login.StepText
 import kr.co.ui.theme.ColorSet.Dream.lightColors
 import kr.co.ui.theme.NBDreamTheme
 import kr.co.ui.theme.typo
+import kr.co.ui.widget.DreamCenterTopAppBar
+import kr.co.ui.widget.NextButton
+import timber.log.Timber
 
+@Composable
+fun InputAddressScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    val savedStateHandle = navController.currentBackStackEntry?.savedStateHandle
+    var fullRoadAddr by remember {
+        mutableStateOf(savedStateHandle?.get<String>("fullRoadAddr") ?: "")
+    }
+    var jibunAddr by remember {
+        mutableStateOf(savedStateHandle?.get<String>("jibunAddr") ?: "")
+    }
 
+    Scaffold(
+        modifier = modifier.padding(16.dp),
+        topBar = {
+            DreamCenterTopAppBar(title = "내 농장 정보")
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            DynamicStepProgressBars(colors = listOf(lightColors.green2, Color.Transparent))
+            StepText("1/2", modifier = Modifier)
+            DescriptionText("내 농장의 주소지를 입력해주세요")
+            Address(
+                fullRoadAddr = fullRoadAddr,
+                jibunAddr = jibunAddr,
+                onFullRoadAddrChange = {
+                    fullRoadAddr = it
+                    savedStateHandle?.set("fullRoadAddr", it)
+                },
+                onJubunAddrChange = {
+                    jibunAddr = it
+                    savedStateHandle?.set("jibunAddr", it)
+                },
+                onSearchClick = {
+                    navController.navigate("ADDRESS_FIND_ROUTE")
+                }
+            )
+            KakaoMapScreen() //padding 없애야 함
+            NextButton()
+        }
+    }
+}
 @Composable
 fun Title(
     title: String,
@@ -122,11 +180,8 @@ private fun Address(
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize()
             .padding()
     ) {
-        StepText("1/2", modifier = Modifier)
-        DescriptionText("내 농장의 주소지를 입력해주세요")
         Row(
             modifier = Modifier
                 .height(IntrinsicSize.Min),
@@ -152,53 +207,99 @@ private fun Address(
                     .border(1.dp, lightColors.secondary, RoundedCornerShape(12.dp))
                     .weight(1f)
                     .height(35.dp)
+
             ) {
                 Text("주소 찾기", style = TextStyle(fontSize = 16.sp, color = lightColors.secondary)) // Adjust text style
             }
         }
-        NextButton()
     }
 }
+
 @Composable
-fun NextButton(
-    modifier: Modifier = Modifier
-) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.BottomCenter
-    ){
-        Column(
-            modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(
-                onClick = {},
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.Transparent,
-                    contentColor = lightColors.secondary
-                ),
-            ) {
-                Text("나중에 입력할게요")
-            }
-            Button(
-                onClick = {},
-                modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(12.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = lightColors.primary,
-                    contentColor = Color.White
-                ),
-            ) {
-                Text(
-                    "다음으로",
-                    style = MaterialTheme.typo.titleSB,
-                )
+fun KakaoMapScreen() {
+    val context = LocalContext.current
+    val apiKey = BuildConfig.KAKAO_API_KEY
+    var mapView by remember { mutableStateOf<MapView?>(null) }
+    var kakaoMap by remember { mutableStateOf<KakaoMap?>(null) }
+    var isSdkInitialized by remember { mutableStateOf(false) }
+    var isMapReady by remember { mutableStateOf(false) }
+
+    //SDK를 초기화
+    LaunchedEffect(apiKey) {
+        try {
+            KakaoMapSdk.init(context, apiKey) //SDK를 초기화
+            isSdkInitialized = true
+            Timber.tag("KakaoMap").d("SDK Initialized successfully")
+        } catch (e: Exception) {
+            Timber.tag("KakaoMap").d("Error initializing SDK: ${e.message}")
+        }
+    }
+
+    //리소스 해제
+    DisposableEffect(Unit) {
+        onDispose {
+            mapView?.let {
+                it.pause() //지도 일시중지
+                Timber.tag("KakaoMap").d("Map paused")
             }
         }
+    }
 
+    //화면에 지도 표시
+    if (isSdkInitialized) { //SDK 초기화 확인
+        AndroidView(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f),
+            factory = { ctx ->
+                MapView(ctx).apply {
+                    mapView = this
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+
+                    if (!isMapReady) {
+                        isMapReady = true
+                        //지도 라이프사이클과 준비 상태를 처리
+                        start(MapLifeCycleCallbackImpl(), KakaoMapReadyCallbackImpl { map ->
+                            kakaoMap = map
+                            Timber.tag("KakaoMap").d("Map is ready")
+                        })
+                    }
+                }
+            },
+            update = {
+                mapView?.resume()
+                Timber.tag("KakaoMap").d("Map resumed")
+            }
+        )
+    } else {
+        // 초기화가 완료되지 않았을 때 보여줄 로딩 UI 또는 메시지
+        Text("Loading map...", modifier = Modifier.padding(16.dp))
     }
 }
+
+class MapLifeCycleCallbackImpl : MapLifeCycleCallback() {
+    override fun onMapDestroy() {
+        Timber.tag("KakaoMap").d("Map is destroyed")
+        // 지도 API가 정상적으로 종료될 때 호출됨
+    }
+
+    override fun onMapError(error: Exception) {
+        Timber.tag("KakaoMap").d("Map error: ${error.message}")
+        // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출됨
+    }
+}
+
+class KakaoMapReadyCallbackImpl(val onMapReady: (KakaoMap) -> Unit) : KakaoMapReadyCallback() {
+    override fun onMapReady(map: KakaoMap) {
+        Timber.tag("KakaoMap").d("Map is ready")
+        // 인증 후 API가 정상적으로 실행될 때 호출됨
+//        onMapReady(map)
+    }
+}
+
 @Composable
 fun LocationSearchScreen(
     navController: NavController,
@@ -239,48 +340,41 @@ fun LocationSearchScreen(
             savedStateHandle?.getLiveData<String>("jibunAddr")?.removeObserver(observer)
         }
     }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Title("내 농장 정보")
-        DynamicStepProgressBars(colors = listOf(lightColors.green2, Color.Transparent))
-        Address(
-            fullRoadAddr = fullRoadAddr,
-            jibunAddr = jibunAddr,
-            onFullRoadAddrChange = { fullRoadAddr = it },
-            onJubunAddrChange = { jibunAddr = it },
-            onSearchClick = {
-                navController.navigate("webview")
-            }
-        )
-    }
 }
 
-
+//주소 찾기 버튼을 눌렀을 때 나오는 WebView
 @Composable
-fun LocationSearchWebViewScreen(navController: NavController, addressSelectionListener: AddressSelectionListener) {
-    AndroidView(
-        factory = { context ->
-            WebView(context).apply {
-                settings.javaScriptEnabled = true
-                settings.domStorageEnabled = true
-                webViewClient = object : WebViewClient() {
-                    override fun onPageFinished(view: WebView?, url: String?) {
-                        loadUrl("javascript:sample2_execDaumPostcode();")
+fun LocationSearchWebViewScreen(
+    addressSelectionListener: AddressSelectionListener
+) {
+    Scaffold(
+        topBar = {
+
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier.padding(paddingValues)
+        ){
+            AndroidView(
+                factory = { context ->
+                    WebView(context).apply {
+                        settings.javaScriptEnabled = true
+                        settings.domStorageEnabled = true
+                        webViewClient = object : WebViewClient() {
+                            override fun onPageFinished(view: WebView?, url: String?) {
+                                loadUrl("javascript:sample2_execDaumPostcode();")
+                            }
+                        }
+                        webChromeClient = WebChromeClient()
+                        addJavascriptInterface(WebAppInterface(addressSelectionListener), "Android")
+                        loadUrl("https://seulseul-35d52.web.app")
                     }
-                }
-                webChromeClient = WebChromeClient()
-                addJavascriptInterface(WebAppInterface(addressSelectionListener), "Android")
-                loadUrl("https://seulseul-35d52.web.app")
-            }
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    )
+                },
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+        }
+    }
 }
 
 interface AddressSelectionListener {
@@ -342,7 +436,17 @@ private fun CustomTextField(
 
 @Preview(showBackground = true)
 @Composable
-private fun DefaultPreview() {
+private fun InputAddressScreenPreview() {
+    NBDreamTheme {
+        InputAddressScreen(
+            modifier = Modifier,
+            navController = NavController(LocalContext.current)
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+private fun DynamicStepProgressBarsPreview() {
     Column(modifier = Modifier.fillMaxWidth()) {
         DynamicStepProgressBars(colors = listOf(lightColors.green2)) // 한 개의 StepProgressBar
         Spacer(modifier = Modifier.height(16.dp))
@@ -356,29 +460,8 @@ private fun DefaultPreview() {
 
 @Preview(showBackground = true)
 @Composable
-private fun NextButtonPreview(){
-    NBDreamTheme {
-        NextButton()
-    }
-}
+private fun AddressPreview(){
+    Address(fullRoadAddr = "사랑시 고백구 행복동", jibunAddr = "jibunAddr", onFullRoadAddrChange = {}, onJubunAddrChange = {}) {
 
-@Composable
-@Preview(showSystemUi = true)
-private fun InputAddressPreview() {
-    NBDreamTheme {
-        Column(
-            Modifier.padding(16.dp)
-        ) {
-            Title("내 농장 정보")
-            DynamicStepProgressBars(colors = listOf(lightColors.green2, Color.Transparent))
-            Address(
-                fullRoadAddr = "",
-                jibunAddr = "",
-                onFullRoadAddrChange = {},
-                onJubunAddrChange = {}
-            ) {
-
-            }
-        }
     }
 }
