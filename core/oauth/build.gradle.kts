1e4758234b7557d07ae66073981ef9c4cd6cdf77
Guri999 <116724657+Guri999@users.noreply.github.com>
2024-06-15 05:10:46 +0900

    Refactor: source -> core module location refactor

---
+++ core/oauth/build.gradle.kts
@@ -0,0 +1,22 @@
+import kr.co.wdtt.convention.Social
+
+plugins {
+    alias(libs.plugins.nbdream.android.library)
+    alias(libs.plugins.nbdream.android.hilt)
+    alias(libs.plugins.sgp)
+}
+
+android {
+    namespace = "kr.co.nbdream.core.oauth"
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(projects.core.domain)
+    implementation(projects.core.common)
+
+    Social()
+}
