2513fc93724d3cddc4c7d5c200ddee716a1fa323
guri999 <zaq8644@gmail.com>
2024-06-17 10:01:44 +0900

    Feat: create Room and UserLocalDataSource

---
+++ core/local/src/main/java/kr/co/local/mapper/UserLocalMapper.kt
@@ -0,0 +1,36 @@
+package kr.co.local.mapper
+
+import kr.co.common.mapper.BaseMapper
+import kr.co.data.model.data.user.UserData
+import kr.co.local.model.UserEntity
+
+internal object UserLocalMapper
+    :BaseMapper<UserData, UserEntity>(){
+    override fun toRight(model: UserData): UserEntity {
+        return with(model) {
+            UserEntity(
+                id = id,
+                name = name,
+                profileImage = profileImage,
+                address = address,
+                latitude = latitude,
+                longitude = longitude,
+                crops = crops
+            )
+        }
+    }
+
+    override fun toLeft(entity: UserEntity): UserData {
+        return with(entity) {
+            UserData(
+                id = id,
+                name = name,
+                profileImage = profileImage,
+                address = address,
+                latitude = latitude,
+                longitude = longitude,
+                crops = crops
+            )
+        }
+    }
+}
