9ad2532e20066e6326024cf2e5a48ffc6ca67874
guri999 <zaq8644@gmail.com>
2024-06-19 06:44:53 +0900

    Feat: 유저 데이터 등록

--- core/domain/src/main/java/kr/co/domain/usecase/user/RegisterUserUseCase.kt
+++ core/domain/src/main/java/kr/co/domain/usecase/user/RegisterUserUseCase.kt
@@ -1,5 +1,6 @@
 package kr.co.domain.usecase.user
 
+import kotlinx.coroutines.flow.first
 import kr.co.domain.entity.UserEntity
 import kr.co.domain.repository.UserRepository
 import kr.co.domain.usecase.SuspendUseCase
@@ -13,6 +14,17 @@ class RegisterUserUseCase @Inject constructor(
     override suspend fun build(params: UserEntity?) {
         checkNotNull(params)
 
-        return userRepository.update(params)
+        return userRepository.fetchLocal().first().let {
+            it.copy(
+                name = params.name?: it.name,
+                address = params.address?: it.address,
+                profileImage = params.profileImage?: it.profileImage,
+                longitude = params.longitude?: it.longitude,
+                latitude = params.latitude?: it.latitude,
+                crops = params.crops.ifEmpty { it.crops }
+            ).let { user ->
+                userRepository.update(user)
+            }
+        }
     }
 }
