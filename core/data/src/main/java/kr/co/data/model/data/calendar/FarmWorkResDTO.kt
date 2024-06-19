d3d09a1d7344e180452de5d7ac66d9f44d08ce42
sunjoolee <sheisthepoem@naver.com>
2024-06-19 15:48:18 +0900

    Feat: Working on farmwork remote data source

---
+++ core/data/src/main/java/kr/co/data/model/data/calendar/FarmWorkResDTO.kt
@@ -0,0 +1,10 @@
+package kr.co.data.model.data.calendar
+
+data class FarmWorkResDTO(
+    val id:String,
+    val startEra:String,
+    val endEra:String,
+    val farmWorkCategory:String,
+    val farmWork:String,
+    val videoUrl:String
+)
