4ed21baf843b6122dc9e42bf4919e78ecc0d6abd
nhkim <nhkim1529@gmail.com>
2024-06-23 11:50:19 +0900

    Feat: 주소 저장하고 불러오는 함수 추가

--- feature/onboard/build.gradle.kts
+++ feature/onboard/build.gradle.kts
@@ -1,3 +1,5 @@
+import kr.co.wdtt.convention.Local
+
 plugins {
     alias(libs.plugins.nbdream.android.feature)
     alias(libs.plugins.nbdream.android.library.compose)
@@ -9,6 +11,7 @@ android {
 
     defaultConfig {
         buildConfigField("String", "KAKAO_API_KEY", "\"kakaoApiKey\"")
+        buildConfigField("String", "DATASTORE_NAME", "\"data_store\"")
     }
     buildFeatures {
         buildConfig = true
@@ -17,4 +20,9 @@ android {
 
 dependencies {
     api(libs.kakao.maps)
+
+    implementation(projects.core.data)
+    implementation(projects.core.common)
+    implementation(libs.kotlinx.serialization.json)
+    Local()
 }
