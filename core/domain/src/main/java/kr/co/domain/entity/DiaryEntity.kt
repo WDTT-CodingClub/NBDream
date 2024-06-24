425529361c0a9a3c1130ed9060bb0d12af5380ce
sunjoolee <sheisthepoem@naver.com>
2024-06-24 13:27:16 +0900

    Refactor: Refactor Diary Entity
    Separate enum class to WorkDescriptionType.kt

--- core/domain/src/main/java/kr/co/domain/entity/DiaryEntity.kt
+++ core/domain/src/main/java/kr/co/domain/entity/DiaryEntity.kt
@@ -1,10 +1,12 @@
 package kr.co.domain.entity
 
+import kr.co.domain.entity.type.CropType
+import kr.co.domain.entity.type.WorkDescriptionType
 import java.time.LocalDate
 
 data class DiaryEntity(
-    val id: String,
-    val crop: CropEntity,
+    val id: Int,
+    val cropType: CropType,
     val registerDate: LocalDate,
     val holidays: List<HolidayEntity> = emptyList(),
     val weatherForecast: WeatherForecastEntity,
@@ -16,35 +18,10 @@ data class DiaryEntity(
     val memo: String = ""
 ) {
     data class WorkDescriptionEntity(
-        val id:String,
-        val type: Type,
+        val id: Int,
+        val type: WorkDescriptionType,
         val description: String,
-    ) {
-        enum class Type {
-            SEED_PREP,
-            SEEDBED_PREP,
-            SOW,
-            GRAFT,
-            BURY_SEEDLING,
-            SEEDBED_MANAGE,
-            STOP_TILLAGE,
-            INITIAL_FERTIL,
-            PLANT_SEEDLING,
-            STILTS_NETS,
-            ADDITIONAL_FERTIL,
-            PEST_CONTROL,
-            WEED,
-            SOIL_COVER,
-            TRIMMING,
-            WATER_MANAGE,
-            HOUSE_MANAGE,
-            TEMP_MANAGE,
-            HARVEST,
-            PACK,
-            SHIP,
-            ETC
-        }
-    }
+    )
 }
 
 
