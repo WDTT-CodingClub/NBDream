42cedd0016204d9fe7e9e6d6dd849203fe5ef10a
sunjoolee <sheisthepoem@naver.com>
2024-06-25 22:23:19 +0900

    Feat: 로컬에서 유저 재배 작물 정보 받아오기

--- core/domain/src/main/java/kr/co/domain/usecase/calendar/GetUserCropsUseCase.kt
+++ core/domain/src/main/java/kr/co/domain/usecase/calendar/GetUserCropsUseCase.kt
@@ -1,30 +1,27 @@
 package kr.co.domain.usecase.calendar
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.transform
 import kr.co.domain.entity.CropEntity
 import kr.co.domain.entity.type.CropType
+import kr.co.domain.repository.UserRepository
 import kr.co.domain.usecase.FlowUseCase
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class GetUserCropsUseCase @Inject constructor(
-//    private val repository: UserRepository
+    private val repository: UserRepository
 ) : FlowUseCase<Unit, List<CropEntity>>() {
-    override fun build(params: Unit?): Flow<List<CropEntity>> =
-//        repository.fetchLocal().transform { userEntity ->
-//            emit(
-//                userEntity.crops.map { crop ->
-//                    CropEntity.getCropEntity(crop)
-//                }
-//            )
-//        }
-        flowOf(
-            listOf(
-                CropEntity(CropType.POTATO),
-                CropEntity(CropType.SWEET_POTATO),
-                CropEntity(CropType.APPLE)
+
+    override fun build(params: Unit?): Flow<List<CropEntity>> {
+        checkNotNull(params)
+        return repository.fetchLocal().transform { userEntity ->
+            emit(
+                userEntity.crops.map { crop ->
+                    CropEntity(CropType.ofValue(crop))
+                }
             )
-        )
+        }
+    }
 }
