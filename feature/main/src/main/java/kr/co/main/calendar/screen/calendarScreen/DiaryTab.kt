a969aef2927dd6bf67fd08f168bdc1297d2f9aac
sunjoolee <sheisthepoem@naver.com>
2024-06-26 11:18:35 +0900

    Feat: 영농일지 캘린더 초기 디자인
    일기 작성된 날짜에 동그라미 표시됨
    streak 표시 구현 전 단계

--- feature/main/src/main/java/kr/co/main/calendar/screen/calendarScreen/DiaryTab.kt
+++ feature/main/src/main/java/kr/co/main/calendar/screen/calendarScreen/DiaryTab.kt
@@ -1,29 +1,21 @@
 package kr.co.main.calendar.screen.calendarScreen
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import kr.co.main.calendar.common.content.CalendarContent
-import kr.co.main.calendar.common.content.CalendarContentWrapper
-import kr.co.main.calendar.common.innerCalendar.InnerCalendar
+import androidx.compose.ui.graphics.toArgb
+import kr.co.main.calendar.common.content.DiaryContent
+import kr.co.main.calendar.screen.calendarScreen.calendar.DiaryCalendar
 import kr.co.main.model.calendar.CropModel
 import kr.co.main.model.calendar.DiaryModel
 import kr.co.main.model.calendar.HolidayModel
-import kr.co.main.providers.FakeDiaryModelProvider
 import kr.co.ui.theme.Paddings
 import kr.co.ui.theme.colors
 import java.time.LocalDate
@@ -37,11 +29,8 @@ internal fun DiaryTab(
     onDateSelect: (LocalDate) -> Unit,
     holidays: List<HolidayModel>,
     diaries: List<DiaryModel>,
-    onEditClick: (Long) -> Unit,
-    onDeleteClick: (Long) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-
     Surface(
         modifier = modifier,
         color = MaterialTheme.colors.gray9
@@ -53,72 +42,36 @@ internal fun DiaryTab(
                     containerColor = Color.White
                 )
             ) {
-                InnerCalendar(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = Paddings.xlarge),
+                DiaryCalendar(
+                    modifier = Modifier.padding(Paddings.xlarge),
+                    cropColor = calendarCrop?.color?.color ?: Color.Transparent.toArgb(),
                     calendarYear = calendarYear,
                     calendarMonth = calendarMonth,
                     selectedDate = selectedDate,
-                    onDateSelect = onDateSelect
+                    onDateSelect = onDateSelect,
+                    holidays = holidays,
+                    diaries = diaries
                 )
             }
-        }
-    }
-}
 
-@Composable
-private fun DiaryList(
-    diaries: List<DiaryModel>,
-    modifier: Modifier = Modifier
-) {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth(),
-        color = Color.LightGray
-    ) {
-        LazyColumn(
-            modifier = modifier,
-            contentPadding = PaddingValues(Paddings.xlarge),
-            verticalArrangement = Arrangement.spacedBy(Paddings.medium)
-        ) {
-            items(diaries, key = { it.id }) {
-                DiaryItem(diary = it)
+            if (calendarCrop == null) {
+                Text(
+                    text = "재배 작물을 추가하고 영농 일지를 작성해 보세요."
+                )
+            }
+            diaries.firstOrNull { it.date == selectedDate }?.let { diary ->
+                Card(
+                    modifier = Modifier.padding(Paddings.large),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White
+                    )
+                ) {
+                    DiaryContent(
+                        modifier = Modifier.padding(Paddings.xlarge),
+                        diary = diary
+                    )
+                }
             }
         }
-    }
-}
-
-@Composable
-private fun DiaryItem(
-    diary: DiaryModel,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier
-    ) {
-        CalendarContentWrapper(
-            modifier = Modifier.padding(Paddings.xlarge),
-            calendarContent = CalendarContent.create(diary)
-        )
-    }
-}
-
-
-@Preview
-@Composable
-private fun DiaryListPreview(
-    @PreviewParameter(FakeDiaryModelProvider::class) diary: DiaryModel
-) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        DiaryList(
-            diaries = listOf(
-                diary.copy(id = 0),
-                diary.copy(id = 1),
-                diary.copy(id = 2),
-                diary.copy(id = 3),
-                diary.copy(id = 4)
-            )
-        )
     }
 }
---
+++ feature/main/src/main/java/kr/co/main/calendar/screen/calendarScreen/calendar/DiaryCalendar.kt
@@ -0,0 +1,212 @@
+package kr.co.main.calendar.screen.calendarScreen.calendar
+
+import androidx.annotation.ColorInt
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import kr.co.common.util.LocalDateUtil
+import kr.co.common.util.iterator
+import kr.co.main.model.calendar.DiaryModel
+import kr.co.main.model.calendar.HolidayModel
+import kr.co.main.model.calendar.type.CropModelColorType
+import kr.co.ui.theme.Paddings
+import kr.co.ui.theme.colors
+import kr.co.ui.theme.typo
+import java.time.DayOfWeek
+import java.time.LocalDate
+
+internal class DiaryCalendarStateHolder(
+    @ColorInt val cropColor: Int,
+    val year: Int,
+    val month: Int,
+    val selectedDate: LocalDate = LocalDate.now(),
+    val onDateSelect: (LocalDate) -> Unit,
+    val holidays: List<HolidayModel> = emptyList(),
+    val diaries: List<DiaryModel> = emptyList()
+) {
+    val startWeekNum = LocalDateUtil.getStartWeekNumber(year, month)
+    val endWeekNum = LocalDateUtil.getEndWeekNumber(year, month)
+}
+
+@Composable
+internal fun DiaryCalendar(
+    @ColorInt cropColor: Int,
+    calendarYear: Int,
+    calendarMonth: Int,
+    selectedDate: LocalDate,
+    onDateSelect: (LocalDate) -> Unit,
+    holidays: List<HolidayModel>,
+    diaries: List<DiaryModel>,
+    modifier: Modifier = Modifier
+) {
+    val stateHolder = rememberDiaryCalendarStateHolder(
+        cropColor,
+        calendarYear,
+        calendarMonth,
+        selectedDate,
+        onDateSelect,
+        holidays,
+        diaries
+    )
+
+    StatelessDiaryCalendar(
+        modifier = modifier,
+        stateHolder = stateHolder,
+    )
+}
+
+@Composable
+private fun rememberDiaryCalendarStateHolder(
+    @ColorInt cropColor: Int,
+    year: Int,
+    month: Int,
+    selectedDate: LocalDate = LocalDate.now(),
+    onDateSelect: (LocalDate) -> Unit,
+    holidays: List<HolidayModel> = emptyList(),
+    diaries: List<DiaryModel> = emptyList()
+) = remember {
+    DiaryCalendarStateHolder(
+        cropColor, year, month, selectedDate, onDateSelect, holidays, diaries
+    )
+}
+
+
+@Composable
+private fun StatelessDiaryCalendar(
+    stateHolder: DiaryCalendarStateHolder,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        CalendarDayOfWeekRow(
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        for (weekNum in stateHolder.startWeekNum..stateHolder.endWeekNum) {
+            val weekDateRange = LocalDateUtil.getWeekDateRange(stateHolder.year, weekNum)
+            Box {
+                DiaryCalendarRow(
+                    cropColor = stateHolder.cropColor,
+                    calendarMonth = stateHolder.month,
+                    weekDateRange = weekDateRange,
+                    selectedDate = stateHolder.selectedDate,
+                    onDateSelect = stateHolder.onDateSelect,
+                    holidays = stateHolder.holidays.filter { it.date in weekDateRange },
+                    diaries = stateHolder.diaries.filter { it.date in weekDateRange }
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun DiaryCalendarRow(
+    @ColorInt cropColor: Int,
+    calendarMonth: Int,
+    weekDateRange: ClosedRange<LocalDate>,
+    selectedDate: LocalDate,
+    onDateSelect: (LocalDate) -> Unit,
+    holidays: List<HolidayModel>,
+    diaries: List<DiaryModel>,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        for (date in weekDateRange) {
+            InnerCalendarDateItem(
+                modifier = Modifier
+                    .weight(1f)
+                    .clickable {
+                        onDateSelect(date)
+                    }
+                    .alpha(
+                        if (date.monthValue == calendarMonth) 1f
+                        else 0.3f
+                    ),
+                cropColor = cropColor,
+                date = date,
+                isSelected = (date == selectedDate),
+                isHoliday = holidays.any { it.date == date } or (date.dayOfWeek == DayOfWeek.SUNDAY),
+                diary = diaries.firstOrNull { it.date == date }
+            )
+        }
+    }
+}
+
+@Composable
+private fun InnerCalendarDateItem(
+    @ColorInt cropColor: Int,
+    date: LocalDate,
+    isSelected: Boolean,
+    isHoliday: Boolean,
+    diary: DiaryModel?,
+    modifier: Modifier = Modifier
+) {
+    val backgroundColor =
+        if (isSelected) Color.Black
+        else if (diary != null) Color(cropColor)
+        else Color.Transparent
+
+    val textColor =
+        if (isHoliday) MaterialTheme.colors.red1
+        else if (isSelected or (diary != null)) Color.White
+        else MaterialTheme.colors.text1
+
+    Box(
+        modifier = modifier
+            .clip(shape = CircleShape)
+            .aspectRatio(1f)
+            .background(backgroundColor)
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(Paddings.medium),
+            text = date.dayOfMonth.toString(),
+            style = MaterialTheme.typo.bodyR,
+            color = textColor,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DiaryCalendarPreview() {
+    var selectedDate by remember {
+        mutableStateOf(LocalDate.now())
+    }
+    StatelessDiaryCalendar(
+        stateHolder = DiaryCalendarStateHolder(
+            cropColor = CropModelColorType.POTATO.color,
+            year = 2024,
+            month = 6,
+            selectedDate = selectedDate,
+            onDateSelect = { selectedDate = it },
+        )
+    )
+}
+
+
+
