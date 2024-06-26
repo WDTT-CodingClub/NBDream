093a99136a57e4b00d4d64144d535a61f3de53bd
sunjoolee <sheisthepoem@naver.com>
2024-06-26 10:14:32 +0900

    Feat: 추가/수정 화면 공통 center top app bar

--- feature/main/src/main/java/kr/co/main/calendar/screen/addScheduleScreen/AddScheduleScreen.kt
+++ feature/main/src/main/java/kr/co/main/calendar/screen/addScheduleScreen/AddScheduleScreen.kt
@@ -1,28 +1,18 @@
 package kr.co.main.calendar.screen.addScheduleScreen
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import kr.co.main.R
-import kr.co.main.model.calendar.type.ScreenModeType
-import kr.co.ui.icon.DreamIcon
-import kr.co.ui.icon.dreamicon.Arrowleft
-import kr.co.ui.theme.colors
-import kr.co.ui.theme.typo
-import kr.co.ui.widget.DreamCenterTopAppBar
+import kr.co.main.calendar.common.AddScreenCenterTopAppBar
 
 @Composable
 internal fun AddScheduleRoute(
@@ -47,11 +37,15 @@ private fun AddScheduleScreen(
     Scaffold(
         modifier = modifier,
         topBar = {
-            AddScheduleTopBar(
+            AddScreenCenterTopAppBar(
                 modifier = Modifier.fillMaxWidth(),
                 screenMode = state.value.screenMode,
+                postModeTitleId = R.string.feature_main_calendar_top_app_bar_add_schedule,
+                editModeTitleId = R.string.feature_main_calendar_top_app_bar_edit_schedule,
                 popBackStack = popBackStack,
-                onActionClick = event::onActionClick
+                onPostClick = event::onPostClick,
+                onEditClick = event::onEditClick,
+                onDeleteClick = event::onDeleteClick
             )
         }
     ) { innerPadding ->
@@ -61,45 +55,8 @@ private fun AddScheduleScreen(
             Column(
                 modifier = Modifier.fillMaxWidth()
             ) {
-
+                // TODO 일정 정보 입력 UI
             }
         }
     }
-}
-
-@Composable
-private fun AddScheduleTopBar(
-    screenMode: ScreenModeType,
-    popBackStack: () -> Unit,
-    onActionClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    DreamCenterTopAppBar(
-        modifier = modifier,
-        title = stringResource(id = R.string.feature_main_calendar_top_app_bar_add_schedule),
-        navigationIcon = {
-            Icon(
-                modifier = Modifier.clickable {
-                    popBackStack()
-                },
-                imageVector = DreamIcon.Arrowleft,
-                contentDescription = ""
-            )
-        },
-        actions = {
-            Text(
-                modifier = Modifier.clickable {
-                    onActionClick()
-                },
-                text = stringResource(
-                    id = when (screenMode) {
-                        ScreenModeType.POST_MODE -> R.string.feature_main_calendar_top_app_bar_post
-                        ScreenModeType.EDIT_MODE -> R.string.feature_main_calendar_top_app_bar_edit
-                    }
-                ),
-                style = MaterialTheme.typo.bodyM,
-                color = MaterialTheme.colors.gray5
-            )
-        }
-    )
 }
