27bf62f22c595ad695ce8af54a440219a0016b78
nhkim <nhkim1529@gmail.com>
2024-06-27 21:01:41 +0900

    Design: Login 화면 로고, 배경 변경

--- feature/onboard/src/main/java/kr/co/onboard/login/Login.kt
+++ feature/onboard/src/main/java/kr/co/onboard/login/Login.kt
@@ -5,18 +5,27 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.co.domain.entity.type.AuthType
+import kr.co.onboard.R
 import kr.co.ui.theme.Paddings
-import kr.co.ui.theme.typo
 import kr.co.ui.widget.DreamSocialButton
 
 @Composable
@@ -26,9 +35,9 @@ internal fun Login(
 ) {
     Box(
         modifier
-            .background(color = Color.White)
-            .padding(Paddings.xlarge),
+            .background(Color(0xFFEFE8E0))
     ) {
+        LogoBackground(modifier = Modifier)
         Logo("농부의 꿈")
         SocialLoginButtons(
             onSocialLoginClick = onSocialLoginClick,
@@ -36,25 +45,78 @@ internal fun Login(
     }
 }
 
+@Composable
+internal fun LogoBackground(
+    modifier: Modifier
+){
+    Row(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Row(
+                modifier = modifier.fillMaxWidth()
+            ){
+                Image(
+                    painter = painterResource(id = kr.co.nbdream.core.ui.R.drawable.backgrounddeco),
+                    contentDescription = "배경 이미지",
+                    modifier = modifier.weight(1f)
+                )
+                Image(
+                    painter = painterResource(id = kr.co.nbdream.core.ui.R.drawable.backgrounddeco),
+                    contentDescription = "배경 이미지",
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+    }
+}
 
 @Composable
 internal fun Logo(
     text: String = "",
 ) {
+    val FontFamily = FontFamily(
+        Font(kr.co.nbdream.core.ui.R.font.nanum_myeongjo_bold, weight = FontWeight.Bold),
+        Font(kr.co.nbdream.core.ui.R.font.nanum_myeongjo, weight = FontWeight.Normal)
+    )
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxSize()
+            .padding(bottom = 20.dp)
     ) {
         Image(
-            painter = painterResource(id = kr.co.nbdream.core.ui.R.drawable.img_logo),
+            painter = painterResource(id = kr.co.nbdream.core.ui.R.drawable.logo),
             contentDescription = "logo"
         )
-        Text(
-            text = text,
-            style = MaterialTheme.typo.displayB
-        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = text,
+                fontFamily = FontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp,
+                color = Color(0xFF413E33)
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = stringResource(R.string.feature_onboard_logo_description),
+                fontFamily = FontFamily,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color(0xFF413E33).copy(0.8f)
+            )
+        }
     }
 }
 
