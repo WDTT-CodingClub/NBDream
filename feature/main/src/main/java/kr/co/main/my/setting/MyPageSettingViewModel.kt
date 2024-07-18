7732aa465b5ef63a6ebd83d608ee567a92c9c268
s-subeen <101384641+s-subeen@users.noreply.github.com>
2024-07-18 15:50:51 +0900

    Feat: Fcm Token 저장, 제거 부분 ViewModel에 추가

--- feature/main/src/main/java/kr/co/main/my/setting/MyPageSettingViewModel.kt
+++ feature/main/src/main/java/kr/co/main/my/setting/MyPageSettingViewModel.kt
@@ -5,22 +5,28 @@ import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import kr.co.domain.usecase.auth.LogoutUseCase
+import kr.co.domain.usecase.fcm.ExpireFcmTokenUseCase
 import kr.co.ui.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 internal class MyPageSettingViewModel @Inject constructor(
     saveStateHandle: SavedStateHandle,
-    private val logoutUseCase: LogoutUseCase
-): BaseViewModel<MyPageSettingViewModel.State>(saveStateHandle) {
+    private val logoutUseCase: LogoutUseCase,
+    private val expireFcmTokenUseCase: ExpireFcmTokenUseCase
+) : BaseViewModel<MyPageSettingViewModel.State>(saveStateHandle) {
 
     fun onLogout() {
         viewModelScopeEH.launch {
             logoutUseCase()
+        }.invokeOnCompletion {
+            viewModelScopeEH.launch {
+                expireFcmTokenUseCase()
+            }
         }
     }
 
-    data object State: BaseViewModel.State
+    data object State : BaseViewModel.State
 
     override fun createInitialState(savedState: Parcelable?) = State
 }
