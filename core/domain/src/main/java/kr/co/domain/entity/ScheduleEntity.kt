52cf1cc7bbd9e799792a2f98679c200b6c8c5ba7
sunjoolee <sheisthepoem@naver.com>
2024-07-19 19:33:44 +0900

    Fix: ScheduleModel에 일정 알림 프로퍼티 추가

--- core/domain/src/main/java/kr/co/domain/entity/ScheduleEntity.kt
+++ core/domain/src/main/java/kr/co/domain/entity/ScheduleEntity.kt
@@ -3,6 +3,7 @@ package kr.co.domain.entity
 import kr.co.domain.entity.type.ScheduleType
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 
 data class ScheduleEntity(
     val id: Long,
@@ -12,7 +13,7 @@ data class ScheduleEntity(
     val endDate: LocalDate = startDate,
     val memo: String = "",
     val isAlarmOn: Boolean = false,
-    val alarmDateTime: LocalDateTime? = null
+    val alarmDateTime: LocalDateTime = LocalDateTime.of(startDate, LocalTime.of(7, 0))
 )
 
 
